8ea9d6134ed353bf6001c5584bbe9a99442ac6a6
Alex <alexayala9975@hotmail.com>
2017-12-07 15:05:27 -0800

    Add localhost to allowed_hosts

--- mainWebsite/settings.py
+++ mainWebsite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = key.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['alexayala.xyz']
+ALLOWED_HOSTS = ['alexayala.xyz', 'localhost', '127.0.0.1']
 
 
 # Application definition
